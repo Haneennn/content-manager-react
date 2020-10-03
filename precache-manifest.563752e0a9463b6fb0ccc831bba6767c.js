8397907967491787af2820f6558ea056c4a444c9
Haneennn <haneenhani861@gmail.com>
2020-10-03 16:00:39 +0500

    Updates

---
+++ precache-manifest.563752e0a9463b6fb0ccc831bba6767c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3f6ef6e4a758ebc3115dda5c23de84",
+    "url": "/content-manager-react/index.html"
+  },
+  {
+    "revision": "c1eaa2bc785eee4ef5b9",
+    "url": "/content-manager-react/static/css/main.4acdc36d.chunk.css"
+  },
+  {
+    "revision": "e3dbbb0a285d1b3cbafb",
+    "url": "/content-manager-react/static/js/2.208bb046.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/content-manager-react/static/js/2.208bb046.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1eaa2bc785eee4ef5b9",
+    "url": "/content-manager-react/static/js/main.1f578459.chunk.js"
+  },
+  {
+    "revision": "15cc5e411b31d0d2a7df",
+    "url": "/content-manager-react/static/js/runtime-main.180ac457.js"
+  }
+]);
